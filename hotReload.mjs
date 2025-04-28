d8c2b951e7b823f1c2738a8860c8b4b8dca459b9
Alkali Metal <alkali.maps@gmail.com>
2025-04-27 21:23:14 -0600

    Create hotReload.mjs

---
+++ hotReload.mjs
@@ -0,0 +1,14 @@
+const loaders = {
+	svg(data) {
+		const iconName = data.path.split(`/`).slice(-1)[0].slice(0, -4);
+		console.debug(`hot-reloading icon: ${iconName}`);
+		Hooks.call(`${game.system.id}-hmr:svg`, iconName, data);
+	},
+	mjs() {window.location.reload()},
+	css() {window.location.reload()},
+};
+
+Hooks.on(`hotReload`, async (data) => {
+	if (!loaders[data.extension]) {return}
+	return loaders[data.extension](data);
+});
